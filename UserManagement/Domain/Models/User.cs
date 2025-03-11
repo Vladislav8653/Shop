fb6bcb7995cfbb590b813e0fc82e2d236c74feae
vlad <arefin.vlad@gmail.com>
2025-03-12 00:26:12 +0300

    Feat: created User model

---
+++ UserManagement/Domain/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserManagement.Domain.Models;
+
+public class User : IdentityUser
+{
+    public string? Role { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpireTime { get; set; }
+}
