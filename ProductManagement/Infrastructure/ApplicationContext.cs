d8bd4a7546d3f4460800e867035bfc12d3d651e9
vlad <arefin.vlad@gmail.com>
2025-03-13 01:04:27 +0300

    Feat: created ProductContext and configuration

---
+++ ProductManagement/Infrastructure/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Domain.Models;
+
+namespace ProductManagement.Infrastructure;
+
+public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
+    }
+    public DbSet<Product> Products { get; set; }
+}
---
+++ ProductManagement/Infrastructure/Configurations/ProductsConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductManagement.Domain.Models;
+
+namespace ProductManagement.Infrastructure.Configurations;
+
+public class ProductsConfig : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.ProductName)
+            .HasMaxLength(100);
+        builder.Property(p => p.Description)
+            .HasMaxLength(1000);
+
+    }
+}
