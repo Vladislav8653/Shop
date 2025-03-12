0a0a9bbc0616e2ac9ad2b1d1e23f77ffe8ff3992
vlad <arefin.vlad@gmail.com>
2025-03-13 00:33:01 +0300

    Feat: created Product model

---
+++ ProductManagement/Domain/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductManagement.Domain.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public bool Available { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
