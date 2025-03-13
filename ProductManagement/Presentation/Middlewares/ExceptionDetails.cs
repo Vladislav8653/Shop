cc0ba8c319c063f37333d3eb3342dc92ab14bd07
vlad <arefin.vlad@gmail.com>
2025-03-14 01:47:23 +0300

    Feat: created global error handler

---
+++ ProductManagement/Presentation/Middlewares/ExceptionDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductManagement.Presentation.Middlewares;
+
+public class ExceptionDetails
+{
+    public ExceptionDetails()
+    {
+        Type = Message = string.Empty;
+    }
+
+    public string Type { get; set; }
+    public string Message { get; set; }
+    
+}
