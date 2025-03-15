54bab69c5c794b608514dbbbb96424ff5d21572c
vlad <arefin.vlad@gmail.com>
2025-03-15 17:56:27 +0300

    Feat: created mapping profile

---
+++ ProductManagement/Application/MappingProfiles/ProductMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductManagement.Application.DTO;
+using ProductManagement.Domain.Models;
+
+namespace ProductManagement.Application.MappingProfiles;
+
+public class ProductMappingProfile : Profile
+{
+    public ProductMappingProfile()
+    {
+        CreateMap<ProductRequestDto, Product>()
+            .ForMember(p => p.CreatedAt, 
+                opt => opt.MapFrom(_ => DateTime.UtcNow));
+        CreateMap<Product, ProductRequestDto>();
+    }
+}
