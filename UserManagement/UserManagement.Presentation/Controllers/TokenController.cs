01fe004956c919487965d6d9414d88a20117d15c
vlad <arefin.vlad@gmail.com>
2025-03-16 18:38:35 +0300

    Feat: created controllers

---
+++ UserManagement/UserManagement.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Application.DTO;
+using UserManagement.Application.UseCases.Commands.TokenCommands.RefreshToken;
+
+namespace UserManagement.Presentation.Controllers;
+
+[Route("api/token")]
+[ApiController]
+public class TokenController(
+    IMediator mediator) : Controller
+{
+    [HttpPost("refresh")]
+    [Authorize]
+    public async Task<IActionResult> Refresh([FromBody] TokenDto tokenDto)
+    {
+        var command = new RefreshTokenCommand
+        {
+            TokenDto = tokenDto
+        };
+        var accessToken = await mediator.Send(command);
+        
+        return Ok(accessToken);
+    }
+}
---
+++ UserManagement/UserManagement.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserManagement.Application.DTO;
+using UserManagement.Application.UseCases.Commands.UserCommands.Authenticate;
+using UserManagement.Application.UseCases.Commands.UserCommands.DeleteById;
+using UserManagement.Application.UseCases.Commands.UserCommands.Register;
+using UserManagement.Application.UseCases.Queries.UserQueries.GetAllUsers;
+
+namespace UserManagement.Presentation.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UsersController(IMediator mediator) : Controller
+{
+    [HttpGet("getAllUsers")]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var query = new GetAllUsersQuery();
+        var users = await mediator.Send(query);
+        
+        return Ok(users);
+    }
+    
+    [HttpPost("register")]
+    public async Task<IActionResult> RegisterUser(
+        [FromBody] RegisterUserDto registerUserDto)
+    {
+        var command = new RegisterUserCommand
+        {
+            RegisterUserDto = registerUserDto
+        };
+        await mediator.Send(command);
+        
+        return Ok();
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Authenticate([FromBody] AuthenticateUserDto userForLogin)
+    {
+        var command = new AuthenticateUserCommand
+        {
+            AuthenticateUserDto = userForLogin
+        };
+        var (accessToken, refreshToken) = await mediator.Send(command);
+         
+        return Ok(new { AccessToken = accessToken, RefreshToken = refreshToken });
+    }
+
+    [HttpDelete("deleteUser/{userId}")]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> DeleteUser(string userId)
+    {
+        var command = new DeleteUserCommand
+        {
+            UserId = userId
+        };
+        await mediator.Send(command);
+        
+        return Ok();
+    }
+}
