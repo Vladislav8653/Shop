14525a2ca34a690eacd4076c8a49b18d5357deb9
vlad <arefin.vlad@gmail.com>
2025-03-16 16:41:53 +0300

    Feat: created ServiceExtensions.cs and installed useful packages

---
+++ UserManagement/UserManagement.Infrastructure/Extensions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UserManagement.Infrastructure.Extensions;
+
+
+public static class ServiceExtensions
+{
+    
+    public static void ConfigureAutoMapper(this IServiceCollection services)
+    {
+        /*services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<ProductMappingProfile>();
+        }, AppDomain.CurrentDomain.GetAssemblies());*/
+    }
+    
+    public static void ConfigureRepository(this IServiceCollection services)
+    {
+        /*services.AddScoped<IProductRepository, ProductRepository>();*/
+    }
+    
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<ApplicationContext>(opts =>
+            opts.UseNpgsql(configuration.GetConnectionString("sqlConnection")));
+    
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        /*services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo { Title = "Inno Shop API", Version = "v1"
+            });
+        });*/
+    }
+    
+    public static void AddValidators(this IServiceCollection services)
+    {
+        /*services.AddValidatorsFromAssemblyContaining<ProductValidator>();*/
+    }
+}
