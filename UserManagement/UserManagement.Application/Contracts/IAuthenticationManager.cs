fbe081e882f46b1b286c8a31cf38eafdaa73a460
vlad <arefin.vlad@gmail.com>
2025-03-16 16:41:18 +0300

    Feat: created auth manager contract

---
+++ UserManagement/UserManagement.Application/Contracts/IAuthenticationManager.cs
@@ -0,0 +1,13 @@
+﻿using UserManagement.Application.DTO;
+using UserManagement.Domain.Models;
+
+namespace UserManagement.Application.Contracts;
+
+public interface IAuthenticationManager
+{
+    Task<bool> ValidateUser(AuthenticateUserDto userForAuth);
+    
+    Task<TokenDto> CreateTokens(User user, bool populateExp);
+    
+    public Task<string> CreateAccessToken(User user);
+}
