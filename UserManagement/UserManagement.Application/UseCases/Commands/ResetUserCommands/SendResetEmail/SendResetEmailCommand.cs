fa537ea9cdd7e45af64ab60fedf7f61aa0a23f14
vlad <arefin.vlad@gmail.com>
2025-03-19 22:51:33 +0300

    Feat: send reset email use case

---
+++ UserManagement/UserManagement.Application/UseCases/Commands/ResetUserCommands/SendResetEmail/SendResetEmailCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using UserManagement.Application.DTO;
+
+namespace UserManagement.Application.UseCases.Commands.ResetUserCommands.SendResetEmail;
+
+public record SendResetEmailCommand : IRequest<string>
+{ 
+    public EmailForPasswordResetDto NewPasswordDto { get; init; }
+}
---
+++ UserManagement/UserManagement.Application/UseCases/Commands/ResetUserCommands/SendResetEmail/SendResetEmailCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using UserManagement.Application.Contracts.ConfirmTokenContracts;
+using UserManagement.Application.Contracts.SmtpContracts;
+using UserManagement.Domain.Models;
+
+namespace UserManagement.Application.UseCases.Commands.ResetUserCommands.SendResetEmail;
+
+public class SendResetEmailCommandHandler(
+    UserManager<User> userManager,
+    ISmtpService smtpService,
+    IConfirmationTokenService confirmationTokenService)
+    : IRequestHandler<SendResetEmailCommand, string>
+{
+    public async Task<string> Handle(SendResetEmailCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByEmailAsync(request.NewPasswordDto.Email);
+        if (user == null)
+        {
+            throw new InvalidOperationException($"User with email {request.NewPasswordDto.Email} not found");
+        }
+        
+        const string subject = "Reset Password";
+        var body = $"Your reset token: {confirmationTokenService.CreateConfirmToken(user.Id)}";
+        await smtpService.SendEmailAsync(
+            user.UserName!, user.Email!, subject, body);
+        return "Reset token sent successfully.";
+    }
+}
